50856844954bbfd507d4aa043bd089cd49d7526f
Yusuf <akuyusuf27@gmail.com>
2020-08-19 23:12:49 +0700

    refactor: 💡 writing page is under construction

--- src/pages/tulisan/index.svelte
+++ src/pages/tulisan/index.svelte
@@ -1,13 +1,9 @@
 <script>
   import SEO from '../../components/seo.svelte';
-
-  const routes = WRITING_ROUTES;
 </script>
 
 <SEO title="Tulisan" />
 
 <h1>Tulisan</h1>
 
-{#each routes as route}
-  <a href="tulisan/{route}">{route}</a>
-{/each}
+<p>Halaman ini sedang dalam tahap pengembangan.</p>
